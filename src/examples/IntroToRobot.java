6c67b866f6b75fd5053f96496ca99aa1fc9c3e82
Jim Galvan <jimmy-galvan@Live.com.mx>
2020-03-31 22:07:55 -0700

    I made the robot draw four squares of different colors

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -8,11 +8,29 @@ public class IntroToRobot {
 		Robot r2d2 = new Robot();
 		r2d2.penDown();
 		r2d2.setPenWidth(20);
-		r2d2.setSpeed(5);
+		r2d2.setSpeed(25);
+		r2d2.setRandomPenColor();
 		for (int i = 0; i < 4; i++) {
 			r2d2.move(100);
 			r2d2.turn(90);
 		}
+		r2d2.setRandomPenColor();
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(100);
+			r2d2.turn(-90);
+		}
+		r2d2.setRandomPenColor();
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(-100);
+			r2d2.turn(-90);
+		}
+		r2d2.setRandomPenColor();
+		r2d2.turn(180);
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(100);
+			r2d2.turn(90);
+		}
+
 
 	}
 }
